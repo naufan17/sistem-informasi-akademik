822240f0b2ab8819759cd201177e17ed4f04a818
hanifamar17 <hanifamar17@gmail.com>
2021-06-21 02:30:42 +0700

    update input-nilai ustadz

--- resources/views/ustadz/input-nilai.blade.php
+++ resources/views/ustadz/input-nilai.blade.php
@@ -28,7 +28,6 @@
                                 </svg>
                             </div>
                             <select class="block appearance-none w-full bg-grey-lighter border border-grey-lighter text-grey-darker py-3 px-4 pr-8 rounded" id="grid-state">
-                                <option>Semua</option>
                                 <option>Awaliyah</option>
                                 <option>1 Wustho</option>
                                 <option>2 Wustho</option>
@@ -40,11 +39,7 @@
                     </div>
                 </div>
 
-
                 <div class="flex space-x-4 items-center pb-8">
-
-                    <!--FLEX 1-->
-                    <!--SET F-NONE AGAR TERKUNCI DAN SET W U/ PANJANG BOXNYA-->
                     <div class="flex-none w-36">
                         <a class="self-center">Mapel</a>
                     </div>
@@ -60,6 +55,32 @@
                             </div>
                             <select class="block appearance-none w-full bg-grey-lighter border border-grey-lighter text-grey-darker py-3 px-4 pr-8 rounded" id="grid-state">
                                 <option>Tafsir</option>
+                            </select>
+                        </div>
+                    </div>
+                </div>
+
+
+                <div class="flex space-x-4 items-center pb-8">
+
+                    <!--FLEX 1-->
+                    <!--SET F-NONE AGAR TERKUNCI DAN SET W U/ PANJANG BOXNYA-->
+                    <div class="flex-none w-36">
+                        <a class="self-center">Semester</a>
+                    </div>
+
+                    <!--FLEX 2-->
+                    <!--SET F-NONE AGAR TERKUNCI DAN SET W U/ PANJANG BOXNYA-->
+                    <div class="flex-none md:w-1/5">
+                        <div class="relative">
+                            <div class="pointer-events-none absolute inset-y-0 right-3 flex items-center text-grey-darker">
+                                <svg class="h-4 w-4" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20">
+                                    <path d="M9.293 12.95l.707.707L15.657 8l-1.414-1.414L10 10.828 5.757 6.586 4.343 8z" />
+                                </svg>
+                            </div>
+                            <select class="block appearance-none w-full bg-grey-lighter border border-grey-lighter text-grey-darker py-3 px-4 pr-8 rounded" id="grid-state">
+                                <option>Ganjil</option>
+                                <option>Genap</option>
                             </select>
                         </div>
                     </div>
@@ -81,10 +102,14 @@
                             <tr>
                                 <th class="text-left py-3 px-4 uppercase font-semibold text-sm">No</th>
                                 <th class="text-left py-3 px-4 uppercase font-semibold text-sm">NIS</th>
-                                <th class="text-left w-1/6 py-3 px-4 uppercase font-semibold text-sm">Nama Santri</th>
-                                <th class="text-left w-1/6 py-3 px-4 uppercase font-semibold text-sm">Tugas</th>
-                                <th class="text-left w-1/6 py-3 px-4 uppercase font-semibold text-sm">Keaktifan</th>
-                                <th class="text-left w-1/6 py-3 px-4 uppercase font-semibold text-sm">Imtihan</th>
+                                <th class="text-left w-1/6 py-3 px-4 uppercase font-semibold text-sm">Nama
+                                    Santri</th>
+                                <th class="text-left w-1/6 py-3 px-4 uppercase font-semibold text-sm">Nilai
+                                    Hasil Belajar</th>
+                                <th class="text-left w-1/6 py-3 px-4 uppercase font-semibold text-sm">Absensi
+                                    Asrama</th>
+                                <th class="text-left w-1/6 py-3 px-4 uppercase font-semibold text-sm">Absensi
+                                    MDNU</th>
                                 <th class="text-left w-1/6 py-3 px-4 uppercase font-semibold text-sm">Total</th>
                             </tr>
                         </thead>
@@ -102,14 +127,14 @@
                                 </td>
                                 <td class="text-left py-3 px-4">
                                     <div class="relative z-0 w-full">
-                                        <input type="text" name="name" placeholder="" required class="pt-3 block w-1/2 bg-transparent border-b appearance-none focus:outline-none focus:ring-0 focus:border-black border-gray-400" />
+                                        <input type="text" name="name" placeholder="(%)" required class="pt-3 block w-1/2 bg-transparent border-b appearance-none focus:outline-none focus:ring-0 focus:border-black border-gray-400" />
                                         <span class="text-sm text-red-600 hidden" id="error">This field is
                                             required</span>
                                     </div>
                                 </td>
                                 <td class="text-left py-3 px-4">
                                     <div class="relative z-0 w-full">
-                                        <input type="text" name="name" placeholder="" required class="pt-3 block w-1/2 bg-transparent border-b appearance-none focus:outline-none focus:ring-0 focus:border-black border-gray-400" />
+                                        <input type="text" name="name" placeholder="(%)" required class="pt-3 block w-1/2 bg-transparent border-b appearance-none focus:outline-none focus:ring-0 focus:border-black border-gray-400" />
                                         <span class="text-sm text-red-600 hidden" id="error">This field is
                                             required</span>
                                     </div>
@@ -130,14 +155,14 @@
                                 </td>
                                 <td class="text-left py-3 px-4">
                                     <div class="relative z-0 w-full">
-                                        <input type="text" name="name" placeholder="" required class="pt-3 block w-1/2 bg-transparent border-b appearance-none focus:outline-none focus:ring-0 focus:border-black border-gray-400" />
+                                        <input type="text" name="name" placeholder="(%)" required class="pt-3 block w-1/2 bg-transparent border-b appearance-none focus:outline-none focus:ring-0 focus:border-black border-gray-400" />
                                         <span class="text-sm text-red-600 hidden" id="error">This field is
                                             required</span>
                                     </div>
                                 </td>
                                 <td class="text-left py-3 px-4">
                                     <div class="relative z-0 w-full">
-                                        <input type="text" name="name" placeholder="" required class="pt-3 block w-1/2 bg-transparent border-b appearance-none focus:outline-none focus:ring-0 focus:border-black border-gray-400" />
+                                        <input type="text" name="name" placeholder="(%)" required class="pt-3 block w-1/2 bg-transparent border-b appearance-none focus:outline-none focus:ring-0 focus:border-black border-gray-400" />
                                         <span class="text-sm text-red-600 hidden" id="error">This field is
                                             required</span>
                                     </div>
@@ -158,14 +183,14 @@
                                 </td>
                                 <td class="text-left py-3 px-4">
                                     <div class="relative z-0 w-full">
-                                        <input type="text" name="name" placeholder="" required class="pt-3 block w-1/2 bg-transparent border-b appearance-none focus:outline-none focus:ring-0 focus:border-black border-gray-400" />
+                                        <input type="text" name="name" placeholder="(%)" required class="pt-3 block w-1/2 bg-transparent border-b appearance-none focus:outline-none focus:ring-0 focus:border-black border-gray-400" />
                                         <span class="text-sm text-red-600 hidden" id="error">This field is
                                             required</span>
                                     </div>
                                 </td>
                                 <td class="text-left py-3 px-4">
                                     <div class="relative z-0 w-full">
-                                        <input type="text" name="name" placeholder="" required class="pt-3 block w-1/2 bg-transparent border-b appearance-none focus:outline-none focus:ring-0 focus:border-black border-gray-400" />
+                                        <input type="text" name="name" placeholder="(%)" required class="pt-3 block w-1/2 bg-transparent border-b appearance-none focus:outline-none focus:ring-0 focus:border-black border-gray-400" />
                                         <span class="text-sm text-red-600 hidden" id="error">This field is
                                             required</span>
                                     </div>
@@ -186,14 +211,14 @@
                                 </td>
                                 <td class="text-left py-3 px-4">
                                     <div class="relative z-0 w-full">
-                                        <input type="text" name="name" placeholder="" required class="pt-3 block w-1/2 bg-transparent border-b appearance-none focus:outline-none focus:ring-0 focus:border-black border-gray-400" />
+                                        <input type="text" name="name" placeholder="(%)" required class="pt-3 block w-1/2 bg-transparent border-b appearance-none focus:outline-none focus:ring-0 focus:border-black border-gray-400" />
                                         <span class="text-sm text-red-600 hidden" id="error">This field is
                                             required</span>
                                     </div>
                                 </td>
                                 <td class="text-left py-3 px-4">
                                     <div class="relative z-0 w-full">
-                                        <input type="text" name="name" placeholder="" required class="pt-3 block w-1/2 bg-transparent border-b appearance-none focus:outline-none focus:ring-0 focus:border-black border-gray-400" />
+                                        <input type="text" name="name" placeholder="(%)" required class="pt-3 block w-1/2 bg-transparent border-b appearance-none focus:outline-none focus:ring-0 focus:border-black border-gray-400" />
                                         <span class="text-sm text-red-600 hidden" id="error">This field is
                                             required</span>
                                     </div>
@@ -203,7 +228,19 @@
                         </tbody>
                     </table>
 
-                    <p class="text-xl py-8 flex items-center">
+                    <!--KETERANGAN TAMBAHAN-->
+                    <div class="bg-white overflow-auto py-8">
+                        <table class="table-auto bg-white py-4">
+                            <tbody class="text-gray-700">
+                                <tr class="bg-gray-200">
+                                    <td class="text-left font-semibold py-3 px-4"><a>Keterangan</a></td>
+                                    <td class="text-left w-1/2 py-3 px-4"><a>: Naik Kelas/Tidak Naik Kelas/-</a></td>
+                                </tr>
+                            </tbody>
+                        </table>
+                    </div>
+
+                    <p class="text-xl flex items-center py-8">
                         Catatan Untuk Santri
                     </p>
                     <div class="bg-white overflow-auto pb-8">
@@ -211,8 +248,10 @@
                             <thead class="bg-gray-800 text-white">
                                 <tr>
                                     <th class="text-left py-3 px-4 uppercase font-semibold text-sm">No</th>
-                                    <th class="text-left w-1/5 py-3 px-4 uppercase font-semibold text-sm">Nama Santri</th>
-                                    <th class="text-left w-full py-3 px-4 uppercase font-semibold text-sm">Catatan</th>
+                                    <th class="text-left w-1/5 py-3 px-4 uppercase font-semibold text-sm">Nama
+                                        Santri</th>
+                                    <th class="text-left w-full py-3 px-4 uppercase font-semibold text-sm">
+                                        Catatan</th>
                                 </tr>
                             </thead>
                             <tbody class="text-gray-700">
@@ -279,38 +318,38 @@
             </div>
 
             <!-- FOOTER -->
-        <footer class="container mx-auto bg-white py-8 border-t border-gray-400">
-            <div class="container flex px-6 py-8 ">
-                <div class="w-full mx-auto flex flex-wrap">
+            <footer class="container mx-auto bg-white py-8 border-t border-gray-400">
+                <div class="container flex px-6 py-8 ">
+                    <div class="w-full mx-auto flex flex-wrap">
 
-                    <div class="text-left flex w-full lg:w-1/2 ">
-                        <div class="px-3 md:px-0">
-                            <h3 class="font-bold text-gray-900">Madrasah Diniyyah Pondok Pesantren Nurul Ummah Yogyakarta</h3>
-                            <a class="inline-block no-underline hover:text-black hover:underline pt-4" href="tel:+62 857 2565 5593">Telp. 0857 2565 5593</a><br>
-                            <a class="inline-block no-underline hover:text-black hover:underline" href="https://web.whatsapp.com/send?phone=6285725655593&text=Assalamualaikum%20Admin">WhatsApp. 0857 2565 5593</a><br>
-                            <a class="inline-block no-underline hover:text-black hover:underline" href="mailto:admisi.nurma@gmail.com">E-mail. admisi.nurma@gmail.com</a>
+                        <div class="text-left flex w-full lg:w-1/2 ">
+                            <div class="px-3 md:px-0">
+                                <h3 class="font-bold text-gray-900">Madrasah Diniyyah Pondok Pesantren Nurul Ummah Yogyakarta</h3>
+                                <a class="inline-block no-underline hover:text-black hover:underline pt-4" href="tel:+62 857 2565 5593">Telp. 0857 2565 5593</a><br>
+                                <a class="inline-block no-underline hover:text-black hover:underline" href="https://web.whatsapp.com/send?phone=6285725655593&text=Assalamualaikum%20Admin">WhatsApp. 0857 2565 5593</a><br>
+                                <a class="inline-block no-underline hover:text-black hover:underline" href="mailto:admisi.nurma@gmail.com">E-mail. admisi.nurma@gmail.com</a>
+                            </div>
                         </div>
-                    </div>
 
-                    <div class="flex w-full lg:w-1/2 lg:justify-end lg:text-right">
-                        <div class="px-3 md:px-0">
-                            <h3 class="font-bold text-gray-900">Media Sosial</h3>
-                            <ul class="list-reset items-center pt-4">
-                                <li>
-                                    <a class="inline-block no-underline hover:text-black hover:underline py-1" href="https://instagram.com/nurulummahyk?igshid=1m74irp80dl0v">Instagram</a>
-                                    <a class="inline-block no-underline hover:text-black hover:underline" href="https://www.youtube.com/channel/UCYQ8dFPzAkFF0WbS0r-IDaw">YouTube</a>
-                                    <a class="inline-block no-underline hover:text-black hover:underline" href="https://www.youtube.com/channel/UCYQ8dFPzAkFF0WbS0r-IDaw">Facebook</a><br>
-                                    <a class="inline-block no-underline hover:text-black hover:underline" href="https://nurulummah.com/">Twitter</a>
-                                    <a class="inline-block no-underline hover:text-black hover:underline" href="https://nurulummah.com/">Website</a>
-                                    
-                                </li>
-                            </ul>
+                        <div class="flex w-full lg:w-1/2 lg:justify-end lg:text-right">
+                            <div class="px-3 md:px-0">
+                                <h3 class="font-bold text-gray-900">Media Sosial</h3>
+                                <ul class="list-reset items-center pt-4">
+                                    <li>
+                                        <a class="inline-block no-underline hover:text-black hover:underline py-1" href="https://instagram.com/nurulummahyk?igshid=1m74irp80dl0v">Instagram</a>
+                                        <a class="inline-block no-underline hover:text-black hover:underline" href="https://www.youtube.com/channel/UCYQ8dFPzAkFF0WbS0r-IDaw">YouTube</a>
+                                        <a class="inline-block no-underline hover:text-black hover:underline" href="https://www.youtube.com/channel/UCYQ8dFPzAkFF0WbS0r-IDaw">Facebook</a><br>
+                                        <a class="inline-block no-underline hover:text-black hover:underline" href="https://nurulummah.com/">Twitter</a>
+                                        <a class="inline-block no-underline hover:text-black hover:underline" href="https://nurulummah.com/">Website</a>
+
+                                    </li>
+                                </ul>
+                            </div>
                         </div>
+
                     </div>
-                    
                 </div>
-            </div>
-        </footer>
+            </footer>
 
         </main>
     </div>
