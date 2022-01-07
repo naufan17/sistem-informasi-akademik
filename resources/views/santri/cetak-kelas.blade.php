e44cdab47ea01f0b880f405a0d15fb2b8828deb6
naufan17 <muhammadnaufan719@yahoo.com>
2022-01-07 20:14:51 +0700

    membuat templating blade cetak pdf

--- resources/views/santri/cetak-kelas.blade.php
+++ resources/views/santri/cetak-kelas.blade.php
@@ -1,40 +1,31 @@
-<!DOCTYPE html>
-<html>
-<head>
-	<title>Daftar Kelas</title>
-	<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
-</head>
-<body>
-	<style type="text/css">
-		table tr td,
-		table tr th{
-			font-size: 9pt;
-		}
-	</style>
-	<center>
-		<h5>Daftar Kelas</h4>
-	</center>	
-    <table class='table table-bordered'>
-        <thead>
-            <tr>
-                <th>No</th>
-                <th>Mata pelajaran</th>
-                <th>Kitab</th>
-                <th>Kelas</th>
-                <th>Jadwal</th>
-            </tr>
-        </thead>
-        <tbody>
-            @foreach($cumulative_studies as $cumulative_study)
-            <tr>
-                <td>{{ $loop->iteration }}</td>
-                <td>{{ $cumulative_study->course }}</td>
-                <td>{{ $cumulative_study->book }}</td>
-                <td>{{ $cumulative_study->grade_number }} {{ $cumulative_study->grade_name }}</td>
-                <td>{{ $cumulative_study->day }}, {{ $cumulative_study->time_begin }} - {{ $cumulative_study->time_end }}</td>
-            </tr>
-            @endforeach
-        </tbody>
-	</table>
-</body>
-</html>
+@extends('layouts.cetak')
+
+@section('content')
+
+<center>
+    <h5>Daftar Kelas</h4>
+</center>	
+<table class='table table-bordered'>
+    <thead>
+        <tr>
+            <th>No</th>
+            <th>Mata pelajaran</th>
+            <th>Kitab</th>
+            <th>Kelas</th>
+            <th>Jadwal</th>
+        </tr>
+    </thead>
+    <tbody>
+        @foreach($cumulative_studies as $cumulative_study)
+        <tr>
+            <td>{{ $loop->iteration }}</td>
+            <td>{{ $cumulative_study->course }}</td>
+            <td>{{ $cumulative_study->book }}</td>
+            <td>{{ $cumulative_study->grade_number }} {{ $cumulative_study->grade_name }}</td>
+            <td>{{ $cumulative_study->day }}, {{ $cumulative_study->time_begin }} - {{ $cumulative_study->time_end }}</td>
+        </tr>
+        @endforeach
+    </tbody>
+</table>
+
+@endsection
